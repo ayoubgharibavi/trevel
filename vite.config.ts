34172cb891f4bb8755fb1309ff2897344001f1ff
Mobinghcrs <mobinghcrs@gmail.com>
2025-08-28 23:25:27 +0330

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyD5Y4X2ewqADYFvArr9YObtLQQTssEEZuc),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyD5Y4X2ewqADYFvArr9YObtLQQTssEEZuc)
       },
       resolve: {
         alias: {
